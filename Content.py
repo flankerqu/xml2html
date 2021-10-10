cb8ea842644cb1c6dd280d4c09c2734dedfc4ab0
flankerqu <flanker.qu@gmail.com>
2021-10-10 22:35:33 +0200

    init

---
+++ Content.py
@@ -0,0 +1,8 @@
+
+class Book:
+
+    def __init__(self, book_id, name, price, description):
+        self.id = book_id
+        self.name = name
+        self.price = price
+        self.description = description
---
+++ reader/XmlReader.py
@@ -0,0 +1,22 @@
+
+from xml.etree import ElementTree as ET
+from Content import Book
+
+class XmlReader:
+
+    def __init__(self, file_path):
+        self.path = file_path
+        self.root = ET.parse(file_path)
+        self.data = None
+    def read(self):
+        self.data = []
+        for b in self.root.findall("book"):
+            self.data.append(Book(b.attrib.get("id"), b.find("genre").text, b.find("price").text, b.find("description").text))
+        return self.data
+
+
+
+path = "../input/books.xml"
+reader = XmlReader(path)
+books = reader.read()
+print(books)
